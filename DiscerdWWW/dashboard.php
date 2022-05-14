a0812ad6f962aeaf0d55b9029415335de2e92a94
Michallesss <mwiemszra@gmail.com>
2022-05-14 13:02:33 +0200

    admin dashboard v.0.1

---
+++ DiscerdWWW/dashboard.php
@@ -0,0 +1,25 @@
+<?php 
+    session_start();
+
+    if((!isset($_SESSION['is_logged'])) || (!$_SESSION['is_logged']) || ($_SESSION['account_permission_level']==0)) {
+        header('Location: index.php');
+        exit();
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Discerd | Admin Dashboard</title>
+        
+        <link rel="stylesheet" href="styles/style.css">
+        <link rel="stylesheet" href="styles/discerd.css">
+        <link rel="icon" href="imgs/icon.ico">
+    </head>
+
+    <body>
+        <h1>Beta admin panel</h1>
+        <a href="discerd.php">Back</a>
+    </body>
+</html>
