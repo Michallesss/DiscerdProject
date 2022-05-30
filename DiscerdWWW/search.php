a992881e490b0008e002d7f36620cf2cc5571c08
Michallesss <mwiemszra@gmail.com>
2022-05-30 21:44:03 +0200

    search style update

--- DiscerdWWW/search.php
+++ DiscerdWWW/search.php
@@ -5,17 +5,33 @@
         header('Location: index.php');
         exit();
     }
+    else {
+        $accountID=$_SESSION['account_accountID'];
+    }
 
     $is_good=false;
     if(isset($_POST['submit'])) {
         if((!isset($_POST['search'])) || ($_POST['search']=="")) {
-            $sr_error="Fill search field";
+            $sr_error="<div class='friendscontent' style='text-align: center;'>Fill search field</div>";
             $is_good=false;
         }
         else {
             $search= "%".$_POST['search']."%";
             $is_good=true;
         }
+    }
+
+    require_once "connect.php";
+    mysqli_report(MYSQLI_REPORT_STRICT);
+    try {
+        $connect = @new mysqli($host, $user, $pass, $database);
+            if($connect->connect_errno!=0) {
+                throw new Exception(mysqli_connect_errno());
+            }
+    }
+    catch(Exception $e) {
+        echo "<i>Error:</i>";
+        echo "<div class='error'><b>Dev info:</b> ".$e."</div>";
     }
 ?><!--W przyszłości dodać jeszcze do tabeli server tagi itd. i filtracje tutaj-->
 
@@ -27,48 +43,125 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Discerd | Search</title>
     
-    <link rel="stylesheet" href="styles/search.css">
     <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/discerd.css">
     <link rel="icon" href="imgs/icon.ico">
 </head>
 
 <body>
-    <div class="topnav">
-        <a href="index.php"><img src="imgs/transparentlogo.png"></a>
-        <div class="search-container">
-            <form action="" method="post">
-                <input type="text" placeholder="Search.." onfocus="this.placeholder=''" onblur="this.placeholder='Search..'" name="search">
-                <input type="submit" value="submit" name="submit">
-            </form>
-            <?php 
-                if(isset($sr_error)) {
-                    echo "<div class='error'>".$sr_error."</div>";
-                    unset($sr_error);
-                }
+    <div class="banner">
+        <a href="discerd.php"><img src="imgs/banner.png"></a>
+        <ol>
+            <?php
+                echo "<li>
+                <form action='' method='post'>
+                    <input type='text' placeholder='Search..' onfocus='this.placeholder=``' onblur='this.placeholder=`Search..`' name='search'>
+                    <input type='submit' value='search' name='submit'>
+                </form>
+                </li>";
             ?>
-        </div>
+        </ol>
     </div>
-    <div class="results">
+    <div class="servers">
+        <?php //list of servers
+            try {
+                if($result = $connect->query(sprintf("SELECT server.serverID, server.server_name, server.server_icon FROM server
+                JOIN server_group_account ON server.serverID=server_group_account.serverID
+                WHERE server_group_account.accountID='$accountID';"))) { 
+                    while($row=$result->fetch_assoc()) {
+                        echo "<div class='servercontent'>";
+                            echo "<a href='server.php?server=".$row['serverID']."'><img alt='".$row['server_name']."' src='usersimgs/".$row['server_icon']."' class='serverimage' title='".$row['server_name']."'></a>";
+                        echo "</div>";
+                    }
+                }
+                else {
+                    throw new Exception($connect->error);
+                }
+            }
+            catch(Exception $e) {
+                echo "<i>Error:</i>";
+                echo "<div class='error'><b>Dev info:</b> ".$e."</div>";
+            }
+        ?>
+    </div>
+    <div class="friends">
+        <?php //list of users
+            try {
+                if($result = $connect->query(sprintf("SELECT `account`.`accountID`, `account`.`nickname`, `account`.`status`, `account`.`activity`, `account`.`aboutme`, `account`.`pfp`, `account`.`banner` FROM account
+                JOIN friendship ON account.accountID=friendship.reciverID
+                WHERE friendship.senderID='$accountID' AND friendship.status!=0
+                UNION
+                SELECT `account`.`accountID`, `account`.`nickname`, `account`.`status`, `account`.`activity`, `account`.`aboutme`, `account`.`pfp`, `account`.`banner` FROM account
+                JOIN friendship ON account.accountID=friendship.senderID
+                WHERE friendship.reciverID='$accountID' AND friendship.status!=0
+                ORDER BY nickname"))) {
+                    while($row=$result->fetch_assoc()) {
+                        switch($row['activity']) {
+                            case 0:
+                                $activity="<span style='color: gray;'>Offline</span>";
+                                break;
+                            case 1:
+                                $activity="<span style='color: green;'>Online</span>";
+                                break;
+                            case 2:
+                                $activity="<span style='color: red;'>Do not distrub</span>";
+                                break;
+                            case 3:
+                                $activity="<span style='color: yellow;'>IDLE</span>";
+                                break;
+                            default:
+                                $activity="<span style='color: gray;'>Offline</span>";
+                                break;
+                        }
+                        echo "<div class='friendslistcontent'>";
+                            echo "<a href='chat.php?chat=".$row['accountID']."'><img src='usersimgs/".$row['pfp']."' class='friendslistimage'>";
+                            echo $row['nickname']."#".$row['accountID']."</a><br>";
+                            echo $activity." ".$row['status'];
+                        echo "</div>";
+                    }
+                }
+                else {
+                    throw new Exception($connect->error);
+                }
+                        
+                if($result = $connect->query(sprintf("SELECT `group`.groupID, `group`.group_name, `group`.group_icon FROM `group`
+                JOIN server_group_account ON `group`.groupID=server_group_account.groupID
+                WHERE server_group_account.accountID='$accountID'"))) {
+                    while($row=$result->fetch_assoc()) {
+                        echo "<div class='friendslistcontent'>";
+                            echo "<a href='group.php?group=".$row['groupID']."'><img src='usersimgs/".$row['group_icon']."' class='friendslistimage'>";
+                            echo $row['group_name']."</a><br>";
+                            echo "<span style='color: gray;'>group</span>";
+                        echo "</div>";
+                    }
+                }
+                else {
+                    throw new Exception($connect->error);
+                }
+            }
+            catch(Exception $e) {
+                echo "<i>Error:</i>";
+                echo "<div class='error'><b>Dev info:</b> ".$e."</div>";
+            }
+        ?>
+    </div>
+    <div class="content">
         <?php
-            require_once "connect.php";
-            mysqli_report(MYSQLI_REPORT_STRICT);
+            if(isset($sr_error)) {
+                echo "<div class='error'>".$sr_error."</div>";
+                unset($sr_error);
+            }
 
             if($is_good) {
                 try {
-                    $connect = @new mysqli($host, $user, $pass, $database);
-                    if($connect->connect_errno!=0) {
-                        throw new Exception(mysqli_connect_errno());
-                    }
-
                     if($result = $connect->query(sprintf("SELECT serverID ,server_name, server_icon FROM `server` WHERE server_name LIKE'%$search%' AND is_public=1"))) {
-                        echo "<ul>";
                         while($row=$result->fetch_assoc()) {
-                            echo "<li>";
+                            echo "<div class='friendscontent'>";
                             $id=$row['serverID'];
-                            echo "<a href='server.php?server=$id'>".$row['server_name']."</a>";
-                            echo "</li>";
+                            echo "<img src='usersimgs/".$row['server_icon']."' class='friendsimage'>";
+                            echo $row['server_name']."<br><a href='invite.php?serverID=".$id."&message='><span style='text-align: right;'>Join</span></a>";
+                            echo "</div>";
                         }
-                        echo "</ul>";
                     }
                     else {
                         throw new Exception($connect->error);
