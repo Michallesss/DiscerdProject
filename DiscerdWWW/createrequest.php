d45788f579cc3a17a7337d28473203c31d65a299
Michallesss <mwiemszra@gmail.com>
2022-05-30 22:27:40 +0200

    createrequests design update

--- DiscerdWWW/createrequest.php
+++ DiscerdWWW/createrequest.php
@@ -5,6 +5,23 @@
         header('Location: index.php');
         exit();
     }
+    else {
+        $accountID=$_SESSION['account_accountID'];
+    }
+
+    require_once "connect.php";
+    mysqli_report(MYSQLI_REPORT_STRICT);
+    try {
+        $connect = @new mysqli($host, $user, $pass, $database);
+        if($connect->connect_errno!=0) {
+            throw new Exception($connect->mysqli_connect_errno());
+        }
+    }
+    catch(Exception $e) {
+        echo "<i>Error:</i>";
+        echo "<div class='error'><b>Dev info:</b> ".$e."</div>";
+        $connect->close();
+    }
     
     if(isset($_POST['submit'])) {
         $is_good=true;
@@ -42,43 +59,31 @@
             }
         }
     
-        require_once "connect.php";
-        mysqli_report(MYSQLI_REPORT_STRICT);
-    
         if($is_good) {
             try {
-                $connect= @new mysqli($host, $user, $pass, $database);
-    
-                if($connect->connect_errno!=0) {
-                    throw new Exception(mysqli_connect_errno());
+                if(!$result = $connect->query("SELECT * FROM account WHERE nickname='$nick' AND accountID='$tag'")) {
+                    throw new Exception($connect->error);
+                }
+
+                $how_many=$result->num_rows;
+                if($how_many<=0) {
+                    $rq_nick_error="That user doesn't exist";
                 }
                 else {
-                    $result = $connect->query("SELECT * FROM account WHERE nickname='$nick' AND accountID='$tag'");
-                    if(!$result) {
+                    if(!$result = $connect->query("SELECT * FROM friendship WHERE (senderID='$senderID' AND reciverID='$reciverID') OR (senderID='$reciverID' AND reciverID='$senderID')")) {
                         throw new Exception($connect->error);
                     }
-
+                    
                     $how_many=$result->num_rows;
-                    if($how_many<=0) {
-                        $rq_nick_error="That user doesn't exist";
+                    if($how_many>0) {
+                        $rq_nick_error="You've already sent an request";
                     }
                     else {
-                        $result = $connect->query("SELECT * FROM friendship WHERE (senderID='$senderID' AND reciverID='$reciverID') OR (senderID='$reciverID' AND reciverID='$senderID')");
-                        if(!$result) {
-                            throw new Exception($connect->error);
-                        }
-                        
-                        $how_many=$result->num_rows;
-                        if($how_many>0) {
-                            $rq_nick_error="You've already sent an request";
+                        if($result=$connect->query("INSERT INTO friendship(`senderID`,`reciverID`,`status`) VALUES ('$senderID','$reciverID','0')")) {
+                            $info = "Request was sent";
                         }
                         else {
-                            if($result=$connect->query("INSERT INTO friendship(`senderID`,`reciverID`,`status`) VALUES ('$senderID','$reciverID','0')")) {
-                                $info = "Request was sent";
-                            }
-                            else {
-                                throw new Exception($connect->error); 
-                            }
+                            throw new Exception($connect->error); 
                         }
                     }
                 }
@@ -102,31 +107,119 @@
     <title>Discer | Send friend request</title>
     
     <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/discerd.css">
     <link rel="stylesheet" href="styles/login.css">
     <link rel="icon" href="imgs/icon.ico">
 </head>
 
 <body>
-    <div class="form">
-        <?php 
-            if(isset($info)) {
-                echo "<div class='info'>".$info."</div>";
-                unset($text);
-                unset($info);
-                echo "<a href='discerd.php' style='float: left;'>Back</a>";
-                exit();
+    <div class="banner">
+        <a href="discerd.php"><img src="imgs/banner.png"></a>
+    </div>
+    <div class="servers">
+        <?php //list of servers
+            try {
+                if($result = $connect->query(sprintf("SELECT server.serverID, server.server_name, server.server_icon FROM server
+                JOIN server_group_account ON server.serverID=server_group_account.serverID
+                WHERE server_group_account.accountID='$accountID';"))) { 
+                    while($row=$result->fetch_assoc()) {
+                        echo "<div class='servercontent'>";
+                            echo "<a href='server.php?server=".$row['serverID']."'><img alt='".$row['server_name']."' src='usersimgs/".$row['server_icon']."' class='serverimage' title='".$row['server_name']."'></a>";
+                        echo "</div>";
+                    }
+                }
+                else {
+                    throw new Exception($connect->error);
+                }
+            }
+            catch(Exception $e) {
+                echo "<i>Error:</i>";
+                echo "<div class='error'><b>Dev info:</b> ".$e."</div>";
             }
         ?>
-        <form action="" method="post">
-            <input type="text" name="text" value="<?php if(isset($text)) echo$text; ?>" placeholder="Nick#1" onfocus="this.placeholder=''" onblur="this.placeholder='Nick#1'">
-            <?php
-                if(isset($rq_nick_error)) {
-                    echo "<div class='error'>".$rq_nick_error."</div>";
-                    unset($rq_nick_error);
-                }?>
-            <input type="submit" name="submit" value="Send request">
-            <a href="discerd.php" style="float: left;">Back</a>
-        </form>
+    </div>
+    <div class="friends">
+        <?php //list of users
+            try {
+                if($result = $connect->query(sprintf("SELECT `account`.`accountID`, `account`.`nickname`, `account`.`status`, `account`.`activity`, `account`.`aboutme`, `account`.`pfp`, `account`.`banner` FROM account
+                JOIN friendship ON account.accountID=friendship.reciverID
+                WHERE friendship.senderID='$accountID' AND friendship.status!=0
+                UNION
+                SELECT `account`.`accountID`, `account`.`nickname`, `account`.`status`, `account`.`activity`, `account`.`aboutme`, `account`.`pfp`, `account`.`banner` FROM account
+                JOIN friendship ON account.accountID=friendship.senderID
+                WHERE friendship.reciverID='$accountID' AND friendship.status!=0
+                ORDER BY nickname"))) {
+                    while($row=$result->fetch_assoc()) {
+                        switch($row['activity']) {
+                            case 0:
+                                $activity="<span style='color: gray;'>Offline</span>";
+                                break;
+                            case 1:
+                                $activity="<span style='color: green;'>Online</span>";
+                                break;
+                            case 2:
+                                $activity="<span style='color: red;'>Do not distrub</span>";
+                                break;
+                            case 3:
+                                $activity="<span style='color: yellow;'>IDLE</span>";
+                                break;
+                            default:
+                                $activity="<span style='color: gray;'>Offline</span>";
+                                break;
+                        }
+                        echo "<div class='friendslistcontent'>";
+                            echo "<a href='chat.php?chat=".$row['accountID']."'><img src='usersimgs/".$row['pfp']."' class='friendslistimage'>";
+                            echo $row['nickname']."#".$row['accountID']."</a><br>";
+                            echo $activity." ".$row['status'];
+                        echo "</div>";
+                    }
+                }
+                else {
+                    throw new Exception($connect->error);
+                }
+                        
+                if($result = $connect->query(sprintf("SELECT `group`.groupID, `group`.group_name, `group`.group_icon FROM `group`
+                JOIN server_group_account ON `group`.groupID=server_group_account.groupID
+                WHERE server_group_account.accountID='$accountID'"))) {
+                    while($row=$result->fetch_assoc()) {
+                        echo "<div class='friendslistcontent'>";
+                            echo "<a href='group.php?group=".$row['groupID']."'><img src='usersimgs/".$row['group_icon']."' class='friendslistimage'>";
+                            echo $row['group_name']."</a><br>";
+                            echo "<span style='color: gray;'>group</span>";
+                        echo "</div>";
+                    }
+                }
+                else {
+                    throw new Exception($connect->error);
+                }
+            }
+            catch(Exception $e) {
+                echo "<i>Error:</i>";
+                echo "<div class='error'><b>Dev info:</b> ".$e."</div>";
+            }
+        ?>
+    </div>
+    <div class="content">
+        <div class="form">
+            <?php 
+                if(isset($info)) {
+                    echo "<div class='info'>".$info."</div>";
+                    unset($text);
+                    unset($info);
+                    echo "<a href='discerd.php' style='float: left;'>Back</a>";
+                    exit();
+                }
+            ?>
+            <form action="" method="post">
+                <input type="text" name="text" value="<?php if(isset($text)) echo$text; ?>" placeholder="Nick#1" onfocus="this.placeholder=''" onblur="this.placeholder='Nick#1'">
+                <?php
+                    if(isset($rq_nick_error)) {
+                        echo "<div class='error'>".$rq_nick_error."</div>";
+                        unset($rq_nick_error);
+                    }?>
+                <input type="submit" name="submit" value="Send request">
+            </form>
+        </div>
     </div>
 </body>
 
